b50dd143ed008ba654bfb47ca3dc9ad2885442a5
PaulZoni <polykovpavel23@gmail.com>
2019-05-09 12:47:32 +0300

    + StepDecay + training monitor

--- pyimagesearch/testmodel/cifar10_lr_decay.py
+++ pyimagesearch/testmodel/cifar10_lr_decay.py
@@ -6,12 +6,12 @@ os.environ['THEANO_FLAGS'] = "device=cuda0,force_device=True,floatX=float32,dnn.
 # gcc.cxxflags=-Wno-narrowing
 import theano
 '''-------------------------------------------'''
-
 import matplotlib
 matplotlib.use("Agg")
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report
 from pyimagesearch.nn.conv.minivggnet import MiniVGGNet
+from keras.callbacks import LearningRateScheduler
 from keras.optimizers import SGD
 from keras.datasets import cifar10
 import matplotlib.pyplot as plt
@@ -19,8 +19,17 @@ import numpy as np
 import argparse
 
 
+def step_decay(epoch):
+    initAlpha = 0.01
+    factor = 0.5
+    dropEvery = 5
+    alpha = initAlpha * (factor ** np.floor((1 + epoch) / dropEvery))
+    return float(alpha)
+
+
 ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", required=False, help="path to the output loss/accuracy plot", default='/home/pavel/Документы/model/cifar10_minivggnet_without_bn.png')
+ap.add_argument("-o", "--output", required=False, help="path to the output loss/accuracy plot",
+                default='/home/pavel/PycharmProjects/nn/pyimagesearch/plot/lr_decay_f0.25_plot.png')
 args = vars(ap.parse_args())
 
 print("[INFO] loading CIFAR-10 data...")
@@ -34,13 +43,12 @@ testY = lb.transform(testY)
 
 labelNames = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
 
-print("[INFO] compiling model...")
-opt = SGD(lr=0.01, decay=0.01 / 40, momentum=0.9, nesterov=True)
+callbacks = [LearningRateScheduler(step_decay)]
+opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
 model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-print("[INFO] training network...")
-H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=64, epochs=40, verbose=1)
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=64, epochs=40, callbacks=callbacks, verbose=1)
 
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=64)
@@ -56,4 +64,5 @@ plt.title("Training Loss and Accuracy on CIFAR-10")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend()
+plt.show()
 plt.savefig(args["output"])
--- pyimagesearch/testmodel/cifar10_monitor.py
+++ pyimagesearch/testmodel/cifar10_monitor.py
@@ -9,51 +9,40 @@ import theano
 
 import matplotlib
 matplotlib.use("Agg")
+from pyimagesearch.callbacks.trainingmonitor import TrainingMonitor
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.metrics import classification_report
+from keras.callbacks import LearningRateScheduler
 from pyimagesearch.nn.conv.minivggnet import MiniVGGNet
+from pyimagesearch.callbacks.StepDecay import step_decay
 from keras.optimizers import SGD
 from keras.datasets import cifar10
-import matplotlib.pyplot as plt
-import numpy as np
 import argparse
+import os
 
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", required=False, help="path to the output loss/accuracy plot", default='/home/pavel/Документы/model/cifar10_minivggnet_without_bn.png')
+ap.add_argument("-o", "--output", required=False, help="path to the output directory",
+                default='/home/pavel/PycharmProjects/nn/pyimagesearch/plot/')
 args = vars(ap.parse_args())
+print("[INFO process ID: {}".format(os.getpid()))
 
 print("[INFO] loading CIFAR-10 data...")
 ((trainX, trainY), (testX, testY)) = cifar10.load_data()
 trainX = trainX.astype("float") / 255.0
 testX = testX.astype("float") / 255.0
-
 lb = LabelBinarizer()
 trainY = lb.fit_transform(trainY)
 testY = lb.transform(testY)
-
 labelNames = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
 
 print("[INFO] compiling model...")
-opt = SGD(lr=0.01, decay=0.01 / 40, momentum=0.9, nesterov=True)
+opt = SGD(lr=0.01, momentum=0.9, nesterov=True)
 model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
-print("[INFO] training network...")
-H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=64, epochs=40, verbose=1)
+figPath = os.path.sep.join([args["output"], "{}_.png".format(os.getpid())])
+jsonPath = os.path.sep.join([args["output"], "{}.json".format(os.getpid())])
 
-print("[INFO] evaluating network...")
-predictions = model.predict(testX, batch_size=64)
-print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=labelNames))
+callbacks = [TrainingMonitor(jsonPath=jsonPath, figPath=figPath), LearningRateScheduler(step_decay)]
 
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, 40), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, 40), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 40), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, 40), H.history["val_acc"], label="val_acc")
-plt.title("Training Loss and Accuracy on CIFAR-10")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend()
-plt.savefig(args["output"])
+model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=64, epochs=40, callbacks=callbacks, verbose=1)
